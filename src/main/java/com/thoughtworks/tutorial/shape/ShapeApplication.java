a4b6275a3d8f0a4d60507dab13ee2eb9c1f34df4
Cintya Aguirre <cintyaaguirre@Cintyas-MacBook-Pro.local>
2020-09-25 18:28:34 -0500

    Initial Commit

---
+++ src/main/java/com/thoughtworks/tutorial/shape/ShapeApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.tutorial.shape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShapeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thoughtworks/tutorial/shape/ShapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.tutorial.shape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
